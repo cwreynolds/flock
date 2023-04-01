7198c11922745b1f8c37e36563b299c1ca11d067
Craig Reynolds <cwr@red3d.com>
2023-04-01 11:02:58 -0700

    Trying to use per-triangle color, continue to get error
    Asked question here: https://stackoverflow.com/questions/75907926/open3d-color-per-triangle

--- flock.py
+++ flock.py
@@ -8,42 +8,35 @@ mesh_vertex_count = 0
 
 mesh_vertex_colors = o3d.utility.Vector3dVector()
 
+def interpolate(alpha, p, q):
+    return (p * (1 - alpha)) + (q * alpha)
 
 def frandom01():
     return np.random.uniform(0, 1)
 
+def frandom2(a, b):
+    return interpolate(frandom01(), a, b)
 
 def add_random_triangle():
-#    pos_scale = 10
     pos_scale = 50
-    add_triangle([pos_scale * frandom01(), pos_scale * frandom01(), 0],
-                  0)
+    add_triangle([pos_scale * frandom01(), pos_scale * frandom01(), 0], None)
 
 def add_triangle(position, orientation):
     xyz = np.array([position[0], position[1], position[2]])
     
     v = np.array([1, 0, 0])
     a = frandom01() * math.pi * 2
-    color = [frandom01(), frandom01(), frandom01()]
-    
-#    print()
-#    print(xyz)
+    color = [frandom2(0.4, 0.6), frandom2(0.4, 0.6), frandom2(0.4, 0.6)]
     
     for i in range(3):
         a += math.pi * 2 / 3
         v = np.array(rotate_xy_about_z(v, a))
-#        print('v = ', v)
         rp = xyz + np.array(v)
-#        print('rp = ', rp)
         mesh_vertices.append(rp)
-        
         mesh_vertex_colors.append(color)
-    
-    global mesh_vertex_count
-    mesh_triangles.append([mesh_vertex_count,
-                           mesh_vertex_count + 1,
-                           mesh_vertex_count + 2])
-    mesh_vertex_count += 3
+
+    t = len(mesh_triangles) * 3
+    mesh_triangles.append([t, t + 1, t + 2])
 
 def rotate_xy_about_z(vec3, angle):
     s = math.sin(angle)
@@ -54,8 +47,6 @@ def rotate_xy_about_z(vec3, angle):
     return [(x * c + y * s), y * c - x * s, z]
 
 def test():
-
-#    for i in range(50):
     for i in range(200):
         add_random_triangle()
 
@@ -63,44 +54,42 @@ def test():
     triangle_mesh = o3d.geometry.TriangleMesh()
     triangle_mesh.vertices = mesh_vertices
     triangle_mesh.triangles = mesh_triangles
-    
-#    mesh_np.vertex_colors = o3d.utility.Vector3dVector(
-#        np.random.uniform(0, 1, size=(N, 3)))
-
     triangle_mesh.vertex_colors = mesh_vertex_colors
-
-#    print('len(mesh_vertices) =', len(mesh_vertices))
-#    print('len(mesh_triangles) =', len(mesh_triangles))
-#    print('Vertices:')
-#    print(np.asarray(triangle_mesh.vertices))
-#    print('Triangles:')
-#    print(np.asarray(triangle_mesh.triangles))
-
-
-    # mesh.triangle["colors"] = o3d.core.Tensor(...)
-#    print('triangle_mesh.triangles["colors"].asarray() =', triangle_mesh.triangles["colors"].asarray() )
-
 
     vis = o3d.visualization.Visualizer()
     vis.create_window()
     vis.add_geometry(triangle_mesh)
     
     rot_z_angle = 0
-
     for i in range(400):
         triangle_mesh.vertices = mesh_vertices
         triangle_mesh.triangles = mesh_triangles
-
         rot_z_angle += 0.008
-    
         rot = triangle_mesh.get_rotation_matrix_from_xyz((0, 0, rot_z_angle))
         triangle_mesh.rotate(rot, center=(0, 0, 0))
-        
-        triangle_mesh.compute_vertex_normals()
-        
+        triangle_mesh.compute_vertex_normals() ## ???
         vis.update_geometry(triangle_mesh)
         vis.poll_events()
         vis.update_renderer()
+    vis.destroy_window()
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Trying to test per-triangle colors as claimed to exist on:
+# https://github.com/isl-org/Open3D/issues/1087#issuecomment-1222782733
+
+def face_color_test():
+    mesh = o3d.geometry.TriangleMesh.create_octahedron()
+    rgb = [[0,0,0],[0,0,1],[0,1,0],[0,1,1],[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
+    face_colors = o3d.utility.Vector3iVector(np.array(rgb, np.int32))
+    mesh.triangles["colors"] = face_colors
+    mesh.compute_vertex_normals()
+
+    # Create a visualization window, add mesh, render.
+    vis = o3d.visualization.Visualizer()
+    vis.create_window()
+    vis.add_geometry(mesh)
+    vis.run()
     vis.destroy_window()
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -116,4 +105,6 @@ if __name__ == "__main__":
 #    print('np.array([1, 2, 3]) + np.array([10, 20, 30])',
 #          np.array([1, 2, 3]) + np.array([10, 20, 30]))
     
-    test()
+#    test()
+
+    face_color_test()
