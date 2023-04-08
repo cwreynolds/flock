accfef6d9c4fc3c67774c81dcd0df6ce7413ed97
Craig Reynolds <cwr@red3d.com>
2023-04-08 07:54:18 -0700

    Add steer() and updateSpeedAndLocalSpace() to Agent.py.
    Temporarily add clip() to Agent.py

--- Agent.py
+++ Agent.py
@@ -14,6 +14,24 @@ import math
 from Vec3 import Vec3
 from LocalSpace import LocalSpace
 
+################################################################################
+# TODO move to a new Utilities.py
+
+# Constrain a given value "x" to be between two bounds: "bound0" and "bound1"
+# (without regard to order). Returns x if it is between the bounds, otherwise
+# returns the nearer bound.
+def clip(x, bound0, bound1):
+    clipped = x
+    minimum = min(bound0, bound1)
+    maximum = max(bound0, bound1)
+    if clipped < minimum:
+        clipped = minimum
+    if clipped > maximum:
+        clipped = maximum
+    return clipped
+################################################################################
+
+
 class Agent:
     """A steerable agent base class."""
 
@@ -32,167 +50,59 @@ class Agent:
     def position(self): return self.ls.p
 
     # Current velocity vector
-    def getVelocity(self): return self.forward() * self.speed
+    def get_velocity(self): return self.forward() * self.speed
 
+    # Advance Agent state forward by time_step while applying steering_force.
+    def steer(self, steering_force, time_step):
+        assert isinstance(steering_force, Vec3), "steering_force must be Vec3."
+        # Limit steering force by max force (simulates power or thrust limit).
+        limit_steering_force = steering_force.truncate(self.max_force)
+        # Adjust force by mass to get acceleration.
+        acceleration = limit_steering_force / self.mass
+        # Update dynamic and gerometric state...
+        self.updateSpeedAndLocalSpace(acceleration * time_step);
+
+    def updateSpeedAndLocalSpace(self, acceleration):
+        new_velocity = self.get_velocity() + acceleration
+        new_speed = new_velocity.length()
+        
+        # TODO 20230407 what if new_speed is zero?
+        #               maybe this should be inside speed>0 block?
+        self.speed = clip(new_speed, 0, self.max_speed)
+        new_forward = new_velocity / new_speed;
+
+        if (self.speed > 0):
+            new_side = new_forward.cross(self.up()).normalize()
+            new_up = new_side.cross(new_forward).normalize()
+            new_position = self.position() + (new_forward * self.speed)
+
+            # TODO assert perpendicular...
+            # ...
+
+            # Set new geometric state.
+            # TODO should these be setters or like in c++ ls().setIJKP()
+            self.ls.x = new_side
+            self.ls.y = new_up
+            self.ls.z = new_forward
+            self.ls.p = new_position
 
     @staticmethod  # This decoration seems to be completely optional,
                    # but for the avoidance of doubt
     def unit_test():
+        a = Agent()
+        force = Vec3(0.1, 0.1, 1)
+        time_step = 1 / 60
+        a.steer(force, time_step)
+        a.steer(force, time_step)
+        a.steer(force, time_step)
+        ref_position = Vec3(0.005, 0.005, 0.10041450153584738)
+    
         ok = True
         ref_ls = LocalSpace()
         ok &= (Agent().side()     == ref_ls.i)
         ok &= (Agent().up()       == ref_ls.j)
         ok &= (Agent().forward()  == ref_ls.k)
         ok &= (Agent().position() == ref_ls.p)
-        ok &= (Agent().getVelocity() == Vec3())
+        ok &= (Agent().get_velocity() == Vec3())
+        ok &= (a.position() == ref_position)
         return ok
-
-
-
-#    class Agent
-#    {
-#    public:
-#
-#
-#        // Advance Agent state forward by time_step while applying steering_force.
-#        void steer(Vec3 steering_force, float time_step)
-#        {
-#            // Limit steering force by max force (simulates power or thrust limit).
-#            Vec3 limit_steering_force = steering_force.truncate(maxForce());
-#            // Adjust force by mass to get acceleration.
-#            Vec3 acceleration = limit_steering_force / mass();
-#            // Update dynamic and gerometric state...
-#            updateSpeedAndLocalSpace(acceleration * time_step);
-#        }
-#        
-#        // TODO need time_step or pre multiply?
-#        void updateSpeedAndLocalSpace(Vec3 acceleration)
-#        {
-#            Vec3 new_velocity = getVelocity() + acceleration;
-#            float new_speed = new_velocity.length();
-#            setSpeed(clip(new_speed, 0, maxSpeed()));
-#            Vec3 new_forward = new_velocity / new_speed;
-#            
-#            if (getSpeed() > 0)
-#            {
-#    //            Vec3 new_side = new_forward.cross(up());
-#    //            Vec3 new_up = new_side.cross(new_forward);
-#                Vec3 new_side = new_forward.cross(up()).normalize();
-#                Vec3 new_up = new_side.cross(new_forward).normalize();
-#
-#                Vec3 new_position = position() + (new_forward * getSpeed());
-#
-#    //            assert(new_side.length() == 1);
-#    //            assert(new_up.length() == 1);
-#    //            assert(new_forward.length() == 1);
-#                
-#                // TODO assert perpendicular...
-#                // ...
-#
-#                // Set new geometric state.
-#                ls().setIJKP(new_side, new_up, new_forward, new_position);
-#            }
-#        }
-#        
-#    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-################################################################################
-
-
-#    //------------------------------------------------------------------------------
-#    //
-#    // Agent.h -- new flock experiments
-#    //
-#    // A steerable agent base class.
-#    //
-#    // MIT License -- Copyright © 2023 Craig Reynolds
-#    //
-#    //------------------------------------------------------------------------------
-#
-#    #pragma once
-#    #include "Vec3.h"
-#    #include "LocalSpace.h"
-#
-#    class Agent
-#    {
-#    public:
-#        // Constructors
-#        Agent() {}
-#        
-#        // Accessors
-#        LocalSpace& ls() { return ls_; }
-#        const LocalSpace& ls() const { return ls_; }
-#        Vec3 side() const { return ls().i(); }
-#        Vec3 up() const { return ls().j(); }
-#        Vec3 forward() const { return ls().k(); }
-#        Vec3 position() const { return ls().p(); }
-#        float mass() const { return mass_; }
-#        float getSpeed() const { return speed_; }
-#        float maxSpeed() { return max_speed_; }
-#        float maxForce() { return max_force_; }
-#        
-#        void setSpeed(float new_speed) { speed_ = new_speed; }
-#        void setMaxSpeed(float max_speed) { max_speed_ = max_speed; }
-#        void setMaxForce(float max_force) { max_force_ = max_force; }
-#        
-#        Vec3 getVelocity() const { return forward() * getSpeed(); }
-#
-#        // Advance Agent state forward by time_step while applying steering_force.
-#        void steer(Vec3 steering_force, float time_step)
-#        {
-#            // Limit steering force by max force (simulates power or thrust limit).
-#            Vec3 limit_steering_force = steering_force.truncate(maxForce());
-#            // Adjust force by mass to get acceleration.
-#            Vec3 acceleration = limit_steering_force / mass();
-#            // Update dynamic and gerometric state...
-#            updateSpeedAndLocalSpace(acceleration * time_step);
-#        }
-#        
-#        // TODO need time_step or pre multiply?
-#        void updateSpeedAndLocalSpace(Vec3 acceleration)
-#        {
-#            Vec3 new_velocity = getVelocity() + acceleration;
-#            float new_speed = new_velocity.length();
-#            setSpeed(clip(new_speed, 0, maxSpeed()));
-#            Vec3 new_forward = new_velocity / new_speed;
-#            
-#            if (getSpeed() > 0)
-#            {
-#    //            Vec3 new_side = new_forward.cross(up());
-#    //            Vec3 new_up = new_side.cross(new_forward);
-#                Vec3 new_side = new_forward.cross(up()).normalize();
-#                Vec3 new_up = new_side.cross(new_forward).normalize();
-#
-#                Vec3 new_position = position() + (new_forward * getSpeed());
-#
-#    //            assert(new_side.length() == 1);
-#    //            assert(new_up.length() == 1);
-#    //            assert(new_forward.length() == 1);
-#                
-#                // TODO assert perpendicular...
-#                // ...
-#
-#                // Set new geometric state.
-#                ls().setIJKP(new_side, new_up, new_forward, new_position);
-#            }
-#        }
-#        
-#    private:
-#        LocalSpace ls_;               // Local coordinate space (pos, orient).
-#        float mass_ = 1;              // Mass, normally ignored as 1.
-#        float speed_ = 0;             // Current forward speed (m/s).
-#        float max_speed_ = 10;        // Speed upper limit (m/s)
-#        float max_force_ = 3;         // Acceleration upper limit (m/s²)
-#    };
