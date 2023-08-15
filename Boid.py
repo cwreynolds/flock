22274911d30ff53650dc77075a5d75ac3421fdf5
Craig Reynolds <cwr@red3d.com>
2023-08-15 16:29:50 -0700

    Thinking about different obstacle avoidance formulation.

--- Boid.py
+++ Boid.py
@@ -226,6 +226,34 @@ class Boid(Agent):
                     Draw.add_line_segment(self.position, path_intersection, c)
         return avoidance
 
+    ############################################################################
+    # TODO 20230815 experiment
+    # version of sphere_avoidance without "if dist_squared < min_dist ** 2:"
+    
+#    def sphere_avoidance_EXPERIMENT(self, min_dist, radius, center):
+#        avoidance = Vec3()
+#        path_intersection = Vec3.ray_sphere_intersection(self.position,
+#                                                         self.forward,
+#                                                         radius, center)
+#        if path_intersection:
+#            # Near enough to require avoidance steering?
+#            dist_squared = (path_intersection - self.position).length_squared()
+#            if dist_squared < min_dist ** 2:
+#                toward_center = center - path_intersection
+#                pure_steering = toward_center.perpendicular_component(self.forward)
+#                avoidance = pure_steering.normalize()
+#                if self.flock.enable_annotation:
+#                    c = Vec3(0.9, 0.7, 0.9) # magenta
+#                    Draw.add_line_segment(self.position, path_intersection, c)
+#        return avoidance
+#
+#    # TODO 20230815 experiment -- thinking about continuous version
+#    def avoidance_strength(min_dist, intersection_dist):
+#        falloff = 1 / (intersection_dist - min(intersection_dist, min_dist))
+#        return 1 - falloff
+
+    ############################################################################
+
     # Bird-like roll control: blends vector toward path curvature center with
     # global up. Overrides method in base class Agent
     def up_reference(self, acceleration):
